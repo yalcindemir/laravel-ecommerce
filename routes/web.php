0c3761cba3d88c9afef31c25fd46384d37c42d77
yalcindemir <yalcindemir86@gmail.com>
2018-02-08 14:41:58 +0300

    Anasayfa Controll yapıldı.

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('merhaba',function (){
-
-   return "Merhaba";
-});
-
-Route::get('/api/v1/merhaba',function (){
-    return ['mesaj' => 'merhaba'];
-});
-
-Route::get('/urun/{urunadi}/{id?}',function ($urunadi,$id=0){
-    return "Ürün Adı: $id $urunadi";
-})->name('urun_detay');
-
-Route::get('/kampanya',function (){
-   return redirect()->route('urun_detay',['urunadi'=>'elma','id'=>5]);
-});
+Route::get('/', 'AnasayfaController@index')->name('anasayfa');
