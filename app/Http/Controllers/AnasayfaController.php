a9ce17d9db8edca04b58ec96a5804812e5c79769
yalcindemir <yalcindemir86@gmail.com>
2018-02-15 13:51:55 +0300

    Controller add

--- app/Http/Controllers/AnasayfaController.php
+++ app/Http/Controllers/AnasayfaController.php
@@ -7,17 +7,6 @@ use Illuminate\Http\Request;
 class AnasayfaController extends Controller
 {
     public function index(){
-        $isim = "Yalçın";
-        $soyisim = "Demir";
-        $isimler = ['Yalçın','Samed','Selami'];
-        $kullanicilar = [
-            ['id'=> 1,'kullanici_adi'=>'Yalçın'],
-            ['id'=> 2,'kullanici_adi'=>'Semra'],
-            ['id'=> 3,'kullanici_adi'=>'Deniz'],
-            ['id'=> 4,'kullanici_adi'=>'Ahmet'],
-            ['id'=> 5,'kullanici_adi'=>'Mehmet']
-        ];
-        //return view('anasayfa')->with(['isim'=>$isim,'soyisim'=>$soyisim]);
-        return view('anasayfa',Compact('isim','soyisim','isimler','kullanicilar'));
+        return view('anasayfa');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'AnasayfaController@index')->name('anasayfa');
 
 Route::get('/kategori/{slug_kategoriadi}','KategoriController@index')->name('kategori');
@@ -18,4 +7,15 @@ Route::get('/kategori/{slug_kategoriadi}','KategoriController@index')->name('kat
 Route::get('/urun/{slug_urunadi}','UrunController@index')->name('urun');
 
 Route::get('/sepet','SepetController@index')->name('sepet');
+
+Route::get('/odeme','OdemeController@index')->name('odeme');
+
+Route::get('/siparisler','SiparisController@index')->name('siparisler');
+
+Route::get('/siparisler/{id}','SiparisController@detay')->name('siparis');
+
+Route::group(['prefix'=>'kullanici'],function (){
+    Route::get('/oturumac','KullaniciController@giris_form')->name('kullanici.oturumac');
+    Route::get('/kaydol','KullaniciController@kayit_form')->name('kullanici.kaydol');
+});
 
